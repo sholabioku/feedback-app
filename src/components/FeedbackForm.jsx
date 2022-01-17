da839acea12cfec2a7caf5543ef44f203f700de6
sholabioku <bilushonline@gmail.com>
2022-01-17 09:05:08 +0100

    Add RatingComponent and its functionality

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import RatingSelect from './RatingSelect';
 import Button from './shared/Button';
 import Card from './shared/Card';
 
@@ -6,6 +7,7 @@ function FeedbackForm() {
   const [text, setText] = useState('');
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
+  const [rating, setRating] = useState(10);
 
   const handleTextChange = (e) => {
     if (text === '') {
@@ -25,7 +27,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/*...@todo rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             type='text'
