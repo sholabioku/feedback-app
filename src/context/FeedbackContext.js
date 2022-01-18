5671766de13262bd700a944aaaa1a0f94cc2edc9
sholabioku <bilushonline@gmail.com>
2022-01-18 09:40:24 +0100

    Add editFeedback event and its functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,10 +7,25 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This text is coming from context',
+      text: 'This is feedback item 1',
       rating: 10,
     },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 7,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 5,
+    },
   ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -23,12 +38,17 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback]);
   };
 
+  const editFeedback = (item) => {
+    setFeedbackEdit({ item, edit: true });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
