a3edfd88b2683e3d2940de431f374cfb715e969d
sholabioku <bilushonline@gmail.com>
2022-01-19 17:56:33 +0100

    Add functionality for updateFeedback from db

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -43,11 +43,17 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback
-  const updateFeedback = (id, updateItem) => {
+  const updateFeedback = async (id, updateItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updateItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updateItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
